1387dc6304094c9ec6abcb805188e0d82727bcfb
chinawutao <462755350@qq.com>
2022-07-07 18:07:50 -0500

    tao wu  7/7/2022 5:55 pm edit

---
+++ backend/src/main/java/com/finalproject/alumnimanagement/Entity/JobAdvertisement.java
@@ -0,0 +1,24 @@
+package com.finalproject.alumnimanagement.Entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class JobAdvertisement {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String description;
+    private String benefits;
+    @OneToMany
+    private List<Tag> tags;
+    private List<String> files;
+}
