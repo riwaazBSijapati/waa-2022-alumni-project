e516dd808b17f2bde7bcc8c2d2ef679e944368ee
Charles C <carpe359@gmail.com>
2022-07-08 14:27:03 -0500

    Work on Thursday Work
    -admin Controller skeleton
    -admin Service
    -Admin Repo
    -created dummy student/faculty pipelines to make admin work
    -Started creating Aspect annotation for CheckingToken, Logger

---
+++ backend/src/main/java/com/finalproject/alumnimanagement/Aspect/LoggerAspect.java
@@ -0,0 +1,10 @@
+package com.finalproject.alumnimanagement.Aspect;
+
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggerAspect {
+}
