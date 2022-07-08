068980e9b2d7d50637a8b5a084fac22b4756fb90
Charles C <carpe359@gmail.com>
2022-07-08 14:26:10 -0500

    Work on Thursday Work
    -admin Controller skeleton
    -admin Service
    -Admin Repo
    -created dummy student/faculty pipelines to make admin work
    -Started creating Aspect annotation for CheckingToken, Logger

--- backend/src/main/java/com/finalproject/alumnimanagement/Service/impl/studentServiceImpl.java
+++ backend/src/main/java/com/finalproject/alumnimanagement/Service/impl/studentServiceImpl.java
@@ -1,10 +1,22 @@
 package com.finalproject.alumnimanagement.Service.impl;
 
 
+import com.finalproject.alumnimanagement.Dto.StudentDto;
 import com.finalproject.alumnimanagement.Service.StudentService;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class StudentServiceImpl implements StudentService {
 
+    @Override
+    public List<StudentDto> getAll() {
+        return null;
+    }
+
+    @Override
+    public StudentDto getById(int id) {
+        return null;
+    }
 }
