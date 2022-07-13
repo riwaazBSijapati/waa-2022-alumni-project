bf82f29b49340348ac2849d0b9f24f9df3209d79
chinawutao1979 <103130005+chinawutao1979@users.noreply.github.com>
2022-07-13 18:16:57 -0500

    Add files via upload
    final code

--- backend/src/main/java/com/finalproject/alumnimanagement/Controller/JobAdvertisementController.java
+++ backend/src/main/java/com/finalproject/alumnimanagement/Controller/JobAdvertisementController.java
@@ -1,14 +1,52 @@
 package com.finalproject.alumnimanagement.Controller;
 
 
+import com.finalproject.alumnimanagement.Dto.JobAdvertisementDto;
+import com.finalproject.alumnimanagement.Dto.JobHistoryDto;
+import com.finalproject.alumnimanagement.Dto.StudentDto;
+import com.finalproject.alumnimanagement.Service.JobAdvertisementService;
+import com.finalproject.alumnimanagement.Service.StudentService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/jobAdvertisement")
+@RequestMapping("/jobAdvertisements")
 @CrossOrigin
 public class JobAdvertisementController {
+
+    @Autowired
+    private JobAdvertisementService jobAdvertisementService;
+//    http://localhost:8080/jobAdvertisements
+    @GetMapping
+    public ResponseEntity<List<JobAdvertisementDto>> getAll(){
+        List<JobAdvertisementDto> result = jobAdvertisementService.getAllJobAdvertisements();
+        if(result.size() >0){
+            return ResponseEntity.ok(result);
+        }
+        return ResponseEntity.badRequest().body(null);
+    }
+    //    http://localhost:8080/jobAdvertisements/4
+    @GetMapping("/{studentId}")
+    public ResponseEntity<List<JobAdvertisementDto>> getAllJobAdvertisementsByStudentId(@PathVariable int studentId){
+        List<JobAdvertisementDto> result = jobAdvertisementService.getAllJobAdvertisementsByStudentId(studentId);
+        if(result.size() >0){
+            return ResponseEntity.ok(result);
+        }
+        return ResponseEntity.badRequest().body(null);
+    }
+    @PostMapping("/addAdvertisement")
+    public String addAdvertisement(@RequestBody JobAdvertisementDto jobAdvertisementDto) {
+        jobAdvertisementService.addAdvertisement(jobAdvertisementDto);
+        return "success";
+    }
+
+    @PostMapping("/search")
+    public List<JobAdvertisementDto> search(@RequestBody JobAdvertisementDto jobAdvertisementDto) {
+        return jobAdvertisementService.search(jobAdvertisementDto);
+    }
 }
