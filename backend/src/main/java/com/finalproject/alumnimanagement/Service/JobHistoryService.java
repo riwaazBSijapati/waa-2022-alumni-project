0f102a89f0e0e956b16f21d6ae85255d80ff85d8
chinawutao1979 <103130005+chinawutao1979@users.noreply.github.com>
2022-07-13 18:21:39 -0500

    Add files via upload
    final code

--- backend/src/main/java/com/finalproject/alumnimanagement/Service/JobHistoryService.java
+++ backend/src/main/java/com/finalproject/alumnimanagement/Service/JobHistoryService.java
@@ -1,5 +1,12 @@
 package com.finalproject.alumnimanagement.Service;
 
 
+import com.finalproject.alumnimanagement.Dto.JobAdvertisementDto;
+import com.finalproject.alumnimanagement.Dto.JobHistoryDto;
+
+import java.util.List;
+
 public interface JobHistoryService {
+    List<JobHistoryDto> getJobHistory();
+    public void addJob(JobHistoryDto jobHistoryDto);
 }
